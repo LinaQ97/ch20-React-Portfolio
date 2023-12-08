d5a2fe60c1cbe915b2b78f2349269174da9ca1f6
LinaQ97 <135244023+LinaQ97@users.noreply.github.com>
2023-12-07 20:33:38 -0500

    added project3 to portfolio

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -5,6 +5,9 @@ export default function Projects() {
             <li><a href="https://dsternthal.github.io/Project1-Travel/">Project1</a></li>
 
             <img src="assets/images/Screenshot 2023-10-02 at 8.39.24 PM.png" class="rounded float-end" alt="..."></img><li><a href="https://github.com/rujainemena/the-tip-jar">Project2</a></li>
+            
+            <img src="assets/images/shredibles.png" class="rounded float-start" alt="..."></img>
+            <li><a href="public/assets/images/shredibles.png">Project3</a></li>
         </section>   
 
     );
